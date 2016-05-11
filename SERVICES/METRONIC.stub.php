b01ac3b61ac55763143a88436bb0ca2e2413f60e
CHGLongStone <jason.medland@gmail.com>
2016-05-11 15:36:35 -0400

    initial stubb add

---
+++ SERVICES/METRONIC.stub.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * METRONIC SUPPORT  
+ * http://keenthemes.com/metronic-theme/
+ *  
+ * 
+ * @author	Jason Medland<jason.medland@gmail.com>
+ * @package	SERVICE\METRONIC\
+ * @subpackage	
+ */
+ 
+ 
+
+namespace SERVICE\METRONIC;
+
+
+/**
+ * Class METRONIC_WRAPPER
+ *
+ * @package SERVICE\METRONIC
+*/
+class METRONIC_WRAPPER { 
+
+	
+	/** 
+	* @param param 
+	* @return return  
+	*/
+	public function __construct($args=null){
+		return;
+	}
+	/**
+	* DESCRIPTOR: an example namespace call 
+	* @param param 
+	* @return return  
+	*/
+	public function init($args=null){
+		return;
+	}
+
+	
+
+}
+
+
+
+?>
